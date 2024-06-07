3b7f12994dad8c639fc455773126b67147b9cd9a
Jan Seidl <janseidl@volny.cz>
2024-06-07 14:42:02 +0200

    new postgres versions

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-PG_VERS=(9.5.15 9.5.16 9.5.17 9.5.18 9.5.19 9.5.20 9.5.21 9.6.15 9.6.16 9.6.17 10.10 10.11 10.12 11.6 11.7 12.1 12.2 13.2)
+PG_VERS=(12.19 13.15 14.12 15.7 16.3)
+DEBIAN=bookworm
 
 for pg_ver in "${PG_VERS[@]}";
 do
@@ -13,16 +14,23 @@ do
 
     major=$(echo ${pg_ver} | sed -E "s/([0-9\.]+)\.[0-9]+$/\1/")
 
-    sed -i -E 's/(FROM postgres):[0-9\.]+/\1:'"$pg_ver"'/' Dockerfile
-    sed -i -E 's/(postgresql-)[0-9\.]+(-hll)/\1'"$major"'\2/' Dockerfile
-
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+      # MacOS
+      sed -i '' -E 's/(FROM postgres):[0-9\.]+.*/\1:'"${pg_ver}-${DEBIAN}"'/' Dockerfile
+      sed -i '' -E 's/(postgresql-)[0-9\.]+(-hll)/\1'"$major"'\2/' Dockerfile
+      sed -i '' -E "s/\w+-pgdg main/${DEBIAN}-pgdg main/" Dockerfile
+    else
+      sed -i -E 's/(FROM postgres):[0-9\.]+.*/\1:'"${pg_ver}-${DEBIAN}"'/' Dockerfile
+      sed -i -E 's/(postgresql-)[0-9\.]+(-hll)/\1'"$major"'\2/' Dockerfile
+      sed -i -E "s/\w+-pgdg main/${DEBIAN}-pgdg main/" Dockerfile
+    fi
     name="jasei/postgres-hll:${pg_ver}"
     docker build -t $name .
     test_cmd="docker run --rm -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -d jasei/postgres-hll:${pg_ver}"
     instance=$($test_cmd)
     echo "TESTING of $instance ($test_cmd)"
     sleep 5;
-    
+
     if docker logs ${instance} | grep "CREATE EXTENSION"
     then
         git add Dockerfile
